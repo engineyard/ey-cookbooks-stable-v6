f359895c4e68839aaf5cebacb52c63546c31d3e2
EY Chef <chef@engineyard.com>
2018-08-29 03:29:11 +0000

    Run prep and build recipes

--- cookbooks/ey-init/recipes/main.rb
+++ cookbooks/ey-init/recipes/main.rb
@@ -4,10 +4,6 @@ Chef::Log.info(ENV.pretty_inspect)
 include_recipe "ey-base::resin_gems"
 include_recipe "ey-core"
 include_recipe "ey-base::bootstrap"
-include_recipe "db_master::prep"
-include_recipe "app::prep"
-include_recipe "lb::prep"
-include_recipe "db_master::build"
-include_recipe "app::build"
-include_recipe "lb::build"
+node.engineyard.instance.roles.each { |role| include_recipe "#{role}::prep" }
+node.engineyard.instance.roles.each { |role| include_recipe "#{role}::build" }
 include_recipe "ey-base::post_bootstrap"
