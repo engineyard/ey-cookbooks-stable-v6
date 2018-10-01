85380d11e2370e12a390e33a2665b0f587f5752c
EY Chef <chef@engineyard.com>
2018-10-01 19:37:35 +0800

    Uninstall all versions of rubygems-update before installing rubygems

--- cookbooks/ruby/recipes/rubygems.rb
+++ cookbooks/ruby/recipes/rubygems.rb
@@ -13,23 +13,8 @@ def ensure_rubygems_version
     if [[ "$(printf "1.5.2\\n$gem_version" | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n | head -n1)" == "1.5.2" ]] ; then gem update --system #{rubygems}; else update_rubygems; fi
     EOF
 
-    # Uninstall previously installed version of Rubygems
-    #  TODO: uninstall ALL versions of rubygems -- the below code won't work if there are multiple versions installed
-    ruby_block "uninstall previous versions of rubygems" do
-      block do
-        output = Mixlib::ShellOut.new('gem list rubygems').run_command.stdout
-        matchdata = output.match(/\(([^\)]*)\)/)
-        if versions = (matchdata && matchdata[1])
-          versions.split(',').each do |version|
-            version.strip!
-             if version != rubygems
-               Mixlib::ShellOut.new('gem uninstall rubygems-update -v #{version}')
-             end
-          end
-        else
-          Mixlib::ShellOut.new('gem install rubygems-update -v #{rubygems} --no-ri --no-rdoc')
-        end
-      end
+    execute "uninstall all rubygems-update versions" do
+      command "gem uninstall --all --executables rubygems-update"
     end
 
     ey_cloud_report "rubygems update" do
