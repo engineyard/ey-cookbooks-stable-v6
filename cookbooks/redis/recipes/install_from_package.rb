bfb19c2d1701841b4f5c56235c4d13de6d4dcc9d
EY Chef <chef@engineyard.com>
2018-10-02 15:11:20 +0800

    Fix redis package name

--- cookbooks/redis/recipes/install_from_package.rb
+++ cookbooks/redis/recipes/install_from_package.rb
@@ -1,13 +1,6 @@
-redis_package = 'dev-db/redis'
 redis_version = node['redis']['version']
 
-enable_package redis_package do
-  version redis_version
-  override_hardmask true
-  unmask :true
-end
-
-package redis_package do
+package "redis" do
   version redis_version
   action :install
 end
