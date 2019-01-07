bde2d9c2fa90861df57d964080b3f82df759946d
EY Chef <chef@engineyard.com>
2019-01-07 16:49:30 +0800

    Clean up ey-core recipe

--- cookbooks/ey-core/recipes/default.rb
+++ cookbooks/ey-core/recipes/default.rb
@@ -13,7 +13,6 @@ end
 include_recipe 'sysctl::tune'
 include_recipe "ey-core::swap"
 
-#TODOv6 include_recipe 'emerge'
 include_recipe 'security_updates'
 
 include_recipe 'ntp'
@@ -23,7 +22,6 @@ include_recipe 'ey-instance-api' # potentially move/absorb into other recipe
 
 include_recipe 'syslog-ng'
 include_recipe 'timezones'
-#nuke?: include_recipe 'atd'
 include_recipe 'logrotate'
 include_recipe 'run-one'
 
