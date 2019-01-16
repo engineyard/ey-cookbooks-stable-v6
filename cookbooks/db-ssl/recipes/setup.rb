fcb558896475056fc90d1f7508847bd8c5ca46b4
EY Chef <chef@engineyard.com>
2019-01-16 19:12:58 +0800

    Fix chef deprecation warnings

--- cookbooks/db-ssl/recipes/setup.rb
+++ cookbooks/db-ssl/recipes/setup.rb
@@ -1,5 +1,5 @@
 case node.engineyard.environment['db_stack_name']
-when /postgres/ 
+when /postgres/
   keyname = 'postgresql'
 when /mysql/
   keyname = 'mysql'
@@ -21,7 +21,7 @@ managed_template "/engineyard/bin/local_key_copy.sh" do
 end
 
 # only on Solo and db_master
-if ['solo','db_master'].include?(node.dna[:instance_role])
+if ['solo','db_master'].include?(node['dna']['instance_role'])
   managed_template "/engineyard/bin/db_server_ssl_keys.sh" do
     owner 'root'
     group 'root'
@@ -29,18 +29,18 @@ if ['solo','db_master'].include?(node.dna[:instance_role])
     source "db_server_ssl_keys.sh.erb"
     variables(
       :db_owner => owner,
-      :db_admin_pass => node.dna['users'].first['password'],
+      :db_admin_pass => node['dna']['users'].first['password'],
       :max_age => 365*5,
       :datadir => datadir,
       :replicas => (node.db_slaves || '').join(' '),
     )
   end
-  
+
   execute "Setup Server SSL CA and root certificate" do
     command "/engineyard/bin/db_server_ssl_keys.sh"
     not_if { File.exists?(File.join(datadir, 'root.crt')) }
   end
-  
+
   managed_template "/engineyard/bin/db_user_ssl_keys.sh" do
     owner 'root'
     group 'root'
@@ -52,19 +52,19 @@ if ['solo','db_master'].include?(node.dna[:instance_role])
       :keyname => keyname
     )
   end
-  
+
   execute "Setup #{owner} user SSL key" do
-    command "/engineyard/bin/db_user_ssl_keys.sh #{owner} #{node.dna['users'].first['password']} #{365*5}"
+    command "/engineyard/bin/db_user_ssl_keys.sh #{owner} #{node['dna']['users'].first['password']} #{365*5}"
     only_if { File.exists?(File.join(datadir, 'root.crt')) }
     not_if { File.exists?(File.join(dbroot, 'keygen', owner, "#{keyname}.key")) }
   end
-  
-  execute "Setup #{node.dna['users'].first['username']} user SSL key" do
-    command "/engineyard/bin/db_user_ssl_keys.sh #{node.dna['users'].first['username']} #{node.dna['users'].first['password']} #{365*5}"
+
+  execute "Setup #{node['dna']['users'].first['username']} user SSL key" do
+    command "/engineyard/bin/db_user_ssl_keys.sh #{node['dna']['users'].first['username']} #{node['dna']['users'].first['password']} #{365*5}"
     only_if { File.exists?(File.join(datadir, 'root.crt')) }
-    not_if { File.exists?(File.join(dbroot, 'keygen', node.dna['users'].first['username'], "#{keyname}.key")) }
+    not_if { File.exists?(File.join(dbroot, 'keygen', node['dna']['users'].first['username'], "#{keyname}.key")) }
   end
-  
+
   # replicas get special handling since sometimes we can't push the keys
   managed_template "/engineyard/bin/remote_key_copy.sh" do
     owner 'root'
@@ -79,12 +79,12 @@ if ['solo','db_master'].include?(node.dna[:instance_role])
       :datadir => datadir
     )
   end
-  
+
   execute "Setup user SSL key on all instances" do
-    command "/engineyard/bin/remote_key_copy.sh #{node.dna['users'].first['username']}"
-    only_if { File.exists?(File.join(dbroot, 'keygen', node.dna['users'].first['username'], "#{keyname}.key")) }
+    command "/engineyard/bin/remote_key_copy.sh #{node['dna']['users'].first['username']}"
+    only_if { File.exists?(File.join(dbroot, 'keygen', node['dna']['users'].first['username'], "#{keyname}.key")) }
   end
-  
+
 
 end
 # needs to copy keys from ebs to home dirs
@@ -94,8 +94,8 @@ execute "Copy db ssl keys for #{owner} from EBS if available" do
     File.exists?(File.join(dbroot, 'keygen', owner, "#{keyname}.key")) }
 end
 
-execute "Copy db ssl keys for #{node.dna['users'].first['username']} from EBS if available" do
-  command "/engineyard/bin/local_key_copy.sh #{node.dna['users'].first['username']}"
+execute "Copy db ssl keys for #{node['dna']['users'].first['username']} from EBS if available" do
+  command "/engineyard/bin/local_key_copy.sh #{node['dna']['users'].first['username']}"
   only_if { File.exists?(File.join(datadir, 'root.crt')) and
-    File.exists?(File.join(dbroot, 'keygen', node.dna['users'].first['username'], "#{keyname}.key")) }
+    File.exists?(File.join(dbroot, 'keygen', node['dna']['users'].first['username'], "#{keyname}.key")) }
 end
