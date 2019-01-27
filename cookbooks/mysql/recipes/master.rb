032814dd4b69a116a27b13cee8ebb4d7c62dda7e
EY Chef <chef@engineyard.com>
2019-01-27 20:06:29 +0800

    Fix command for setting root password on mysql 5.7

--- cookbooks/mysql/recipes/master.rb
+++ cookbooks/mysql/recipes/master.rb
@@ -22,10 +22,19 @@ end
 
 include_recipe "mysql::startup"
 
+set_root_mysql_password = case node['mysql']['short_version']
+  when '5.6'
+    %Q{
+      /usr/bin/mysqladmin -u root password '#{node.engineyard.environment['db_admin_password']}' || /usr/bin/mysqladmin -u root --password='' password '#{node.engineyard.environment['db_admin_password']}'; true
+    }
+  when '5.7'
+    %Q{
+      mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '#{node.engineyard.environment['db_admin_password']}'"; true
+    }
+  end
+
 execute "set-root-mysql-pass" do
-  command %Q{
-    /usr/bin/mysqladmin -u root password '#{node.engineyard.environment['db_admin_password']}' || /usr/bin/mysqladmin -u root --password='' password '#{node.engineyard.environment['db_admin_password']}'; true
-  }
+  command set_root_mysql_password
 end
 
 include_recipe "mysql::cleanup" if node['mysql']['short_version'] == '5.6' # MySQL 5.7 doesn't include extra users/databases by default
