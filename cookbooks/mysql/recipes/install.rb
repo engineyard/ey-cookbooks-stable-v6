812a8b823632fd0579b56ad33f95a81090ba5674
EY Chef <chef@engineyard.com>
2019-02-07 15:49:16 +0800

    Install deb files to support mysql lock db feature

--- cookbooks/mysql/recipes/install.rb
+++ cookbooks/mysql/recipes/install.rb
@@ -1,15 +1,16 @@
-=begin TODOv6 lock_db
 lock_db_version = node.engineyard.environment.components.find_all {|e| e['key'] == 'lock_db_version'}.first['value'] if node.engineyard.environment.lock_db_version?
 
 lock_version_file = '/db/.lock_db_version'
 db_running = %x{mysql -N -e "select 1;" 2> /dev/null}.strip == '1'
 
 known_versions = {
-  # Note: mysql 5.5 is a limited access feature on this stack; use 5.6 or higher if possible.
-  'dev-db/mysql' => ['5.5.49'],
-  'dev-db/percona-server' => ['5.6.28.76.1', '5.6.29.76.2-r1', '5.6.32.78.1', '5.6.35.81.0', '5.6.37.82.2', '5.7.13.6', '5.7.14.8', '5.7.17.13', '5.7.19.17']
+  '5.7.24' => 'https://www.percona.com/downloads/Percona-Server-5.7/Percona-Server-5.7.24-27/binary/debian/bionic/x86_64/Percona-Server-5.7.24-27-rbd42700-bionic-x86_64-bundle.tar',
+  '5.7.23' => 'https://www.percona.com/downloads/Percona-Server-5.7/Percona-Server-5.7.23-25/binary/debian/bionic/x86_64/Percona-Server-5.7.23-25-r7e2732e-bionic-x86_64-bundle.tar',
+  '5.7.22' => 'https://www.percona.com/downloads/Percona-Server-5.7/Percona-Server-5.7.22-22/binary/debian/bionic/x86_64/Percona-Server-5.7.22-22-rf62d93c-bionic-x86_64-bundle.tar',
+  '5.7.21' => 'https://www.percona.com/downloads/Percona-Server-5.7/Percona-Server-5.7.21-21/binary/debian/bionic/x86_64/Percona-Server-5.7.21-21-r2a37e4e-bionic-x86_64-bundle.tar'
 }
 
+# create or delete /db/.lock_db_version
 if node.dna['instance_role'][/^(db|solo)/]
   execute "dropping lock version file" do
     command "echo $(mysql --version | grep -E -o 'Distrib [0-9]+\.[0-9]+\.[0-9]+' | awk '{print $NF}') > #{lock_version_file}"
@@ -23,82 +24,44 @@ if node.dna['instance_role'][/^(db|solo)/]
   end
 end
 
-unmask_package "virtual/mysql" do
-  version node['mysql']['virtual']
-  unmaskfile "mysql"
+# check if the version is valid
+if File.exists?(lock_version_file)
+  install_version  = %x{cat #{lock_version_file}}.strip
+else
+  install_version = node['mysql']['latest_version']
+end
+package_url = known_versions[install_version]
+
+if package_url.nil?
+  Chef::Log.info "Chef does not know about MySQL version #{install_version}"
+  exit(1)
+else
+  Chef::Log.info "lock_db_version: #{lock_db_version}, Installing: #{install_version}"
 end
 
-enable_package "virtual/mysql" do
-  version node['mysql']['virtual']
+# download the tar file from Percona
+remote_file "/tmp/src/Percona-Server-#{install_version}.tar" do
+  source package_url
 end
 
-enable_package "virtual/libmysqlclient" do
-  version '20'
-  only_if { node['mysql']['short_version'] == "5.7" }
+directory "delete Percona src directory" do
+  path "/tmp/src/Percona-Server-#{install_version}"
+  action :delete
+  recursive true
 end
 
-ey_cloud_report "mysql" do
-  message "Handling MySQL Install"
+directory "create Percona src directory" do
+  path "/tmp/src/Percona-Server-#{install_version}"
+  action :create
 end
 
-ruby_block "getting full version and doing install" do    # ~FC014
-  # FoodCritic note: Ignore as this is used in one place and it is clearer described here
-  block do
-    if File.exists?(lock_version_file)
-      install_version  = %x{cat #{lock_version_file}}.strip
-    else
-      install_version = node['mysql']['latest_version']
-    end
-    package_name, package_version = nil
-    known_versions.each do |label, versions|
-      if match = versions.detect {|v| v =~ /^#{install_version}/}
-        package_name = label
-        package_version = match
-        break
-      end
-    end
-
-    if package_version.nil?
-      Chef::Log.info "Chef does not know about MySQL version #{install_version}"
-      exit(1)
-    else
-      Chef::Log.info "--- lock_db_version = #{lock_db_version} -- Installing: #{package_version}"
-      keyword_file = '/etc/portage/package.keywords/local'
-      unmask_file = '/etc/portage/package.unmask/mysql'
-      %x{ grep -q "=#{package_name}-#{package_version}" #{keyword_file} || echo "=#{package_name}-#{package_version}" >> #{keyword_file}}
-      %x{ grep -q "=#{package_name}-#{package_version}" #{unmask_file} || echo "=#{package_name}-#{package_version}" >> #{unmask_file}}
-      %x{ grep -q "=dev-util/cmake-2.6.2" #{keyword_file} || echo "=dev-util/cmake-2.6.2" >> #{keyword_file}}
-      %x{ emerge -g --noreplace =#{package_name}-#{package_version}}
-    end
-  end
+execute "extract Percona" do
+  command "tar xvf /tmp/src/Percona-Server-#{install_version}.tar -C /tmp/src/Percona-Server-#{install_version}"
 end
 
-include_recipe "mysql::relink_mysql" unless node['mysql']['short_version'] == "5.6"
-
-if node.dna['instance_role'][/^db/]    # ~FC023
-  # FoodCritic note: Ignore becuase this custom resource doesn't offer only_if
-  sysctl "Set vm.swappiness" do
-    variables 'vm.swappiness' => '15'
-  end
-end
-=end
-
-lsb_codename = node['lsb']['codename']
-execute "fetch percona packages" do
-  command "wget https://repo.percona.com/apt/percona-release_0.1-6.$(lsb_release -sc)_all.deb"
-  cwd "/tmp/src"
-  not_if { File.exists?("/tmp/src/percona-release_0.1-6.#{lsb_codename}_all.deb") }
-end
-
-execute "apt-get update for percona" do
-  command "apt-get update"
-  action :nothing
-end
-
-execute "add percona repositories" do
-  command "dpkg -i /tmp/src/percona-release_0.1-6.#{lsb_codename}_all.deb"
-  not_if { File.exists?("/etc/apt/sources.list.d/percona-release.list") }
-  notifies :run, "execute[apt-get update for percona]", :immediately
+# install the dependencies of the Percona packages
+%w[debsums libaio1 libmecab2].each do |package|
+  package package
 end
 
 package "libmysqlclient-dev"
@@ -119,7 +82,23 @@ if node['dna']['instance_role'][/db|solo/]
     notifies :run, "execute[reload-systemd]", :immediately
   end
 
-  package "percona-server-server-#{node['mysql']['short_version']}"
+  packages = %w[percona-server-common percona-server-client percona-server-server]
 else
-  package "percona-server-client-#{node['mysql']['short_version']}"
+  packages = %w[percona-server-common percona-server-client]
+end
+
+packages.each do |package|
+  execute "install #{package}" do
+    command %Q{
+      installed=$(apt-cache policy #{package}-#{node['mysql']['short_version']} | grep "Installed: #{install_version}-")
+      if [ -z $installed ]
+      then
+        echo 'Installing #{package}-#{node['mysql']['short_version']}'
+        dpkg -i /tmp/src/Percona-Server-#{install_version}/#{package}*.deb
+      else
+        echo '#{package}-#{node['mysql']['short_version']} is already installed'
+      fi
+    }
+    environment({"DEBIAN_FRONTEND" => "noninteractive"})
+  end
 end
