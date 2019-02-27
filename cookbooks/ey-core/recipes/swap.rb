57e5c0cb89ea9eb3b78056dda022eeaf5c70c5c7
Johann Fuechsl <johann@fuechsl.co>
2019-02-27 10:48:15 +0100

    Wait for swap partition to exist before running mkswap

--- cookbooks/ey-core/recipes/swap.rb
+++ cookbooks/ey-core/recipes/swap.rb
@@ -2,6 +2,9 @@ bash "make-swap-xvdc" do
   code <<-EOH
     parted -s -a optimal /dev/xvdc mklabel msdos
     parted -s -a optimal -- /dev/xvdc unit compact mkpart primary linux-swap "1" "-1"
+    while [[ ! -b /dev/xvdc1 ]]; do
+      sleep 1
+    done
     mkswap /dev/xvdc1
     swapon /dev/xvdc1
     echo "/dev/xvdc1 swap swap sw 0 0" >> /etc/fstab
@@ -9,6 +12,7 @@ bash "make-swap-xvdc" do
   only_if { File.exists?("/dev/xvdc") && !system("grep -q '/dev/xvdc1' /etc/fstab") && (`blkid /dev/xvdc1 -o value -s TYPE` !~ /^swap/) }
 end
 
+## TODOv6 remove this as on v6 we certainly wont support c1 and m1
 # xvda3 is provided by the AWS IaaS on m1.small and c1.medium by default
 bash "use-ec2-swap-partition" do
   code <<-EOH
