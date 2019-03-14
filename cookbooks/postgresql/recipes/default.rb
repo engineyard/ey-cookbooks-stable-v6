ef5f36143ed82b5570665b4d6b5e2b4dbaebbe2d
EY Chef <chef@engineyard.com>
2019-03-14 19:10:22 +0800

    Include postgresql recipes

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,11 +5,11 @@ if ['solo', 'db_master', 'db_slave'].include?(node.dna['instance_role'])
     message "processing postgresql #{postgres_version}"
   end
   include_recipe "ebs::default"
-  #TODOv6 include_recipe "postgresql::client_config"
+  include_recipe "postgresql::client_config"
   include_recipe "postgresql::server_install"
   include_recipe "postgresql::server_configure"
   #TODOv6 include_recipe "postgresql::monitoring"
-  #TODOv6  include_recipe "postgresql::relink_postgresql"
+  include_recipe "postgresql::relink_postgresql"
   ey_cloud_report "stop postgresql run" do
     message "processing postgresql #{postgres_version} finished"
   end
@@ -27,5 +27,5 @@ if ['app_master', 'app', 'util'].include?(node.dna['instance_role'])
 end
 
 if (db_host_is_rds? && node.dna[:instance_role] == 'app_master') || (!db_host_is_rds? && ['solo', 'db_master', 'eylocal'].include?(node.dna[:instance_role]))
-  #TODOv6 include_recipe "postgresql::setup_app_users_dbs"
+  include_recipe "postgresql::setup_app_users_dbs"
 end
