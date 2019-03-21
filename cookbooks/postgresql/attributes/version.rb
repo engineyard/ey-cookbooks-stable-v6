4e89b969c9e86b840b0e9c4257c45fc261ff67b5
EY Chef <chef@engineyard.com>
2019-03-21 22:56:01 +0800

    Add postgresql lock version feature

--- cookbooks/postgresql/attributes/version.rb
+++ cookbooks/postgresql/attributes/version.rb
@@ -1,15 +1,15 @@
 case attribute.dna.engineyard.environment.db_stack_name
 when "postgres9_4"
-  default['postgresql']['latest_version'] = '9.4.12'
+  default['postgresql']['latest_version'] = '9.4.21'
   default['postgresql']['short_version'] = '9.4'
 when "postgres9_5"
-  default['postgresql']['latest_version'] = '9.5.7'
+  default['postgresql']['latest_version'] = '9.5.16'
   default['postgresql']['short_version'] = '9.5'
 when "postgres9_6"
-  default['postgresql']['latest_version'] = '9.6.3'
+  default['postgresql']['latest_version'] = '9.6.12'
   default['postgresql']['short_version'] = '9.6'
 when "postgres10"
-  default['postgresql']['latest_version'] = '10.4'
+  default['postgresql']['latest_version'] = '10.7'
   default['postgresql']['short_version'] = '10'
 end
 default['postgresql']['datadir'] = "/db/postgresql/#{node['postgresql']['short_version']}/data/"
