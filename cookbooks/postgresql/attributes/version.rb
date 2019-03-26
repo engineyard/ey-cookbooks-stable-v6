a7435b79cc674183b1899e29c12629e6a2899cf4
EY Chef <chef@engineyard.com>
2019-03-26 17:59:00 +0800

    Only postgresql 9.5 and 9.6 are supported in 9.x

--- cookbooks/postgresql/attributes/version.rb
+++ cookbooks/postgresql/attributes/version.rb
@@ -1,7 +1,4 @@
 case attribute['dna']['engineyard']['environment']['db_stack_name']
-when "postgres9_4"
-  default['postgresql']['latest_version'] = '9.4.21'
-  default['postgresql']['short_version'] = '9.4'
 when "postgres9_5"
   default['postgresql']['latest_version'] = '9.5.16'
   default['postgresql']['short_version'] = '9.5'
