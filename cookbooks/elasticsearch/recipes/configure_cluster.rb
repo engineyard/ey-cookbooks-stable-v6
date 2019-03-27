231a8b820f965f9f77c6992b4f700a9f945a56f9
Johann Fuechsl <johann@fuechsl.co>
2019-03-27 09:04:46 +0100

    Syntax error

--- cookbooks/elasticsearch/recipes/configure_cluster.rb
+++ cookbooks/elasticsearch/recipes/configure_cluster.rb
@@ -7,7 +7,7 @@ else
   elasticsearch_expected = 0
   node.dna['utility_instances'].each do |elasticsearch|
     if elasticsearch['name'].include?("elasticsearch")
-      unless node.dna['fqdn'] == elasticsearch['hostname'] do
+      unless node.dna['fqdn'] == elasticsearch['hostname']
         elasticsearch_expected = elasticsearch_expected + 1
         elasticsearch_instances << "#{elasticsearch['hostname']}:9300"
       end
