9917dade888b5f5f9e4c6c6f6f5ff1f8f619a22e
Dimitris Dalianis <ddalianis@engineyard.com>
2019-04-09 13:11:15 +0300

    V5 porting of commit: https://github.com/engineyard/ey-cookbooks-stable-v5/commit/e082789a740e9fe6f5c245e760855e368a2cd178

--- cookbooks/ey-init/recipes/integrate.rb
+++ cookbooks/ey-init/recipes/integrate.rb
@@ -8,6 +8,8 @@ when 'util'
 when /^db/
   if node.engineyard.environment['db_stack_name'][/^postgres/]
     include_recipe 'ey-backup::postgres'
+    is_db_master = ['db_master'].include?(node.dna['instance_role'])
+    include_recipe "db-ssl::setup" if is_db_master
   end
 end
 include_recipe "ssh_keys" # CC-691 - update ssh whitelist after takeovers
