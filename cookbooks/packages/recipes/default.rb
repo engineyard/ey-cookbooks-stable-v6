6eef933fd5d2d86074c5573278ba322212ee384e
EY Chef <chef@engineyard.com>
2019-04-26 11:06:18 +0800

    Add key and source to apt

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -3,7 +3,20 @@
 # Recipe:: default
 #
 
-Chef::Log.info "PACKAGES: #{node['packages']}"
+node['packages']['keys'].each do |key|
+  execute "add key from #{key['url']}" do
+    command "curl -sS #{key['url']} | apt-key add -"
+  end
+end
+
+node['packages']['apt_sources'].each do |apt_source|
+  file "apt source #{apt_source['name']}" do
+    path "/etc/apt/sources.list.d/#{apt_source['name']}.list"
+    content apt_source["content"]
+    notifies :run, "execute[update-apt]", :immediately
+  end
+end
+
 node['packages']['install'].each do |package|
 
   Chef::Log.info "PACKAGES: Installing #{package['name']}-#{package['version']}"
--- custom-cookbooks/packages/cookbooks/custom-packages/attributes/default.rb
+++ custom-cookbooks/packages/cookbooks/custom-packages/attributes/default.rb
@@ -9,3 +9,29 @@ default['packages'].tap do |packages|
     {'name' => "wkhtmltopdf"}
   ]
 end
+
+# To install a package from a different repository, you need to add the key and source to apt.
+# For example, to install yarn from their own repository, their instructions tell you to run the following commands on the command line
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+#
+# Instead of running those commands, you can use the recipe to add the key and source and to install the package.
+# 1) add the URL of the gpg key to 'keys'
+# 2) add the name and content of the apt source to 'apt_sources'
+# 3) add the package to 'install'
+
+=begin
+default['packages'].tap do |packages|
+  packages['keys'] = [
+    {"url" => "https://dl.yarnpkg.com/debian/pubkey.gpg"}
+  ]
+
+  packages['apt_sources'] = [
+    {"name" => "yarn", "content" => "deb https://dl.yarnpkg.com/debian/ stable main"}
+  ]
+
+  packages['install'] = [
+    {'name' => 'yarn'}
+  ]
+end
+=end
