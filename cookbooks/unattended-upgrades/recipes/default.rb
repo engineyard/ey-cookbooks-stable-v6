e31900dd05c7e5875f2b2a826b23d770fe5a049a
Dimitris Dalianis <ddalianis@engineyard.com>
2019-05-14 17:54:04 +0300

    Enable Ubuntu's unattended upgrades if customer select so

---
+++ cookbooks/unattended-upgrades/recipes/default.rb
@@ -0,0 +1,21 @@
+unattended_flag = 0
+
+node['dna']['engineyard']['environment']['apps'].each do |app_data|
+
+  environment_variables = fetch_environment_variables(app_data)
+
+  environment_variables.each do |variable|
+    if variable[:name] == 'EY_UNATTENDED_UPGRADES' && variable[:value] == 'true'
+      unattended_flag = 1
+    end
+  end
+end
+
+template "/etc/apt/apt.conf.d/20auto-upgrades" do
+  mode 0644
+  source "auto-upgrades.erb"
+  backup false
+  variables({
+    :unattended_flag => unattended_flag
+  })
+end
