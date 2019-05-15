07c57216692b778e5cf80fecf4d632f6248015ea
Dimitris Dalianis <ddalianis@engineyard.com>
2019-05-15 14:48:12 +0300

    Enable security upgrades only if switch is on

--- cookbooks/unattended-upgrades/recipes/default.rb
+++ cookbooks/unattended-upgrades/recipes/default.rb
@@ -19,3 +19,10 @@ template "/etc/apt/apt.conf.d/20auto-upgrades" do
     :unattended_flag => unattended_flag
   })
 end
+
+cookbook_file "/etc/apt/apt.conf.d/50unattended-upgrades" do
+  source '50unattended-upgrades'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
