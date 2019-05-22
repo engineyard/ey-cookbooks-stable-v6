457e2f0bdeace906861b0df8bb008554eff6b0ba
EY Chef <chef@engineyard.com>
2019-05-22 01:53:25 +0000

    Add php 7.3

--- cookbooks/php/attributes/php.rb
+++ cookbooks/php/attributes/php.rb
@@ -1,16 +1,14 @@
-default['php']['full_atom'] = "dev-lang/php"
-
 default['php']['version'] = case attribute['dna']['engineyard']['environment']['components'].map(&:values).flatten.find(/^php_/).first
-  when 'php_56'
-    '5.6.30'
   when 'php_7'
-    '7.0.11'
+    '7.0'
   when 'php_71'
-    '7.1.0'
+    '7.1'
   when 'php_72'
-    '7.2.8'
+    '7.2'
+  when 'php_73'
+    '7.3'
   else
-   '5.6.30'
+    '7.2'
 end
- 
+
 default['php']['minor_version'] =  default['php']['version'].split(".").first(2).join(".")
