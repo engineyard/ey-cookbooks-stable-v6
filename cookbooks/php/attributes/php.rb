05ae62e4d8bdcaa8807df80a666d7a79d63f24f7
EY Chef <chef@engineyard.com>
2019-05-22 01:58:30 +0000

    Remove php 7.0

--- cookbooks/php/attributes/php.rb
+++ cookbooks/php/attributes/php.rb
@@ -1,6 +1,4 @@
 default['php']['version'] = case attribute['dna']['engineyard']['environment']['components'].map(&:values).flatten.find(/^php_/).first
-  when 'php_7'
-    '7.0'
   when 'php_71'
     '7.1'
   when 'php_72'
