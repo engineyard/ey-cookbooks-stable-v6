8e9dd9a4d1a08a9c247eff34f91363583f00f04a
EY Chef <chef@engineyard.com>
2019-05-22 01:49:57 +0000

    Add PHP ppa

--- cookbooks/php/recipes/install.rb
+++ cookbooks/php/recipes/install.rb
@@ -3,4 +3,12 @@ ey_cloud_report "processing php" do
   message "processing php - install"
 end
 
-package "php"
+version = node["php"]["minor_version"]
+
+execute "add repository" do
+  command "add-apt-repository ppa:ondrej/php"
+  not_if { File.exist?("/etc/apt/sources.list.d/ondrej-ubuntu-php-bionic.list") }
+end
+
+package "php#{version}"
+package "php#{version}-mbstring"
