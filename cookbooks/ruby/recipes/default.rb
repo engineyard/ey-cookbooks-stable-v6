74996fa22d5182e0f6ad4cefcd8d6ba26db1a18c
EY Chef <chef@engineyard.com>
2019-06-25 05:12:09 +0000

    Fetch ruby version from EY_RUBY

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -28,7 +28,10 @@ remote_file "/tmp/src/ruby-install-0.7.0.tar.gz" do
   notifies :run, "execute[extract-ruby-install]", :immediately
 end
 
-ruby_version = node[:ruby][:version]
+env_var_ruby = fetch_env_var(node, "EY_RUBY")
+ruby_version = env_var_ruby.nil? ? node[:ruby][:version] : env_var_ruby
+log "Setting Ruby version to #{ruby_version}"
+
 template "/etc/profile.d/chruby.sh" do
   source "chruby.sh.erb"
   owner "root"
