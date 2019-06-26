c648d96d737de55020178301aaac71c6cebfbf32
EY Chef <chef@engineyard.com>
2019-06-26 06:59:30 +0000

    Don't overwrite redis.conf when installing redis

--- cookbooks/redis/recipes/install_from_package.rb
+++ cookbooks/redis/recipes/install_from_package.rb
@@ -3,4 +3,5 @@ redis_version = node['redis']['version']
 package "redis" do
   version redis_version
   action :install
+  options "-o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\""
 end
