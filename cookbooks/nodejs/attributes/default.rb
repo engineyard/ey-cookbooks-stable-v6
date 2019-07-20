5a2f195001fb4de63c5f93b01d61c461513e5753
EY Chef <chef@engineyard.com>
2019-07-20 07:57:36 +0000

    Add nodejs 10.16.0

--- cookbooks/nodejs/attributes/default.rb
+++ cookbooks/nodejs/attributes/default.rb
@@ -7,6 +7,8 @@ version = begin
           end
 
 fallback_nodejs_version = case version
+                          when 'nodejs_10'
+                            '10.16.0'
                           when 'nodejs_9'
                             '9.11.2'
                           when 'nodejs_8'
