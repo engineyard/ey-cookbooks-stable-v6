a0de3a6378f74407fecabd620bfd132c1986e6d4
Callum Pease <cpease@engineyard.com>
2019-07-28 18:52:33 +0100

    fixed old urls for papertrail main

--- cookbooks/papertrail/metadata.rb
+++ cookbooks/papertrail/metadata.rb
@@ -5,5 +5,5 @@ version '6.0.0'
 maintainer 'Engine Yard, Inc.'
 maintainer_email 'support@engineyard.com'
 
-issues_url 'https://github.com/engineyard/ey-cookbooks-stable-v6/issue'
+issues_url 'https://github.com/engineyard/ey-cookbooks-stable-v6/issues'
 source_url 'https://github.com/engineyard/ey-cookbooks-stable-v6'
