015b05cc3b6f969e930607c661ca97a4b1e85b6c
Callum Pease <cpease@engineyard.com>
2019-07-28 18:51:28 +0100

    created papertrail main recipe

--- cookbooks/papertrail/metadata.rb
+++ cookbooks/papertrail/metadata.rb
@@ -1,9 +1,9 @@
 name 'papertrail'
 
-version '5.0.0'
+version '6.0.0'
 
 maintainer 'Engine Yard, Inc.'
 maintainer_email 'support@engineyard.com'
 
-issues_url 'https://github.com/engineyard/ey-cookbooks-stable-v5/issue'
-source_url 'https://github.com/engineyard/ey-cookbooks-stable-v5'
+issues_url 'https://github.com/engineyard/ey-cookbooks-stable-v6/issue'
+source_url 'https://github.com/engineyard/ey-cookbooks-stable-v6'
