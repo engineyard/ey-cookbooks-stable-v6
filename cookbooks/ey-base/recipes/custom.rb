85090f1a10e39d8dc6477698212b58b7837179b6
EY Chef <chef@engineyard.com>
2019-07-31 08:48:42 +0000

    Rename environment variable to EY_MEMCACHED_ENABLED

--- cookbooks/ey-base/recipes/custom.rb
+++ cookbooks/ey-base/recipes/custom.rb
@@ -2,6 +2,6 @@ if fetch_env_var(node, "EY_REDIS_ENABLED") =~ /^TRUE$/i
   include_recipe 'redis'
 end
 
-if fetch_env_var(node, "EY_MEMCACHED")
+if fetch_env_var(node, "EY_MEMCACHED_ENABLED") =~ /^TRUE$/i
   include_recipe 'memcached'
 end
