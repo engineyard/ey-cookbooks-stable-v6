a0b2ec3dfb42fc0cb77d97fbcf8f15725e348041
Johann Fuechsl <johann@fuechsl.co>
2019-08-08 09:47:39 +0200

    Remove versions in the newrelic_infra recipes

--- cookbooks/newrelic_infra/attributes/default.rb
+++ cookbooks/newrelic_infra/attributes/default.rb
@@ -1,4 +1,3 @@
 default['newrelic_infra']['use_newrelic_addon'] = false
 default['newrelic_infra']['license_key'] = "INSERT_NEW_RELIC_INFRA_KEY"
-default['newrelic_infra']['package_version'] = "1.3.3"
 default['newrelic_infra']['display_name'] = nil
