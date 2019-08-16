f1c175f12f6d5f1f36c9a50668876c026fa2c31e
EY Chef <chef@engineyard.com>
2019-08-16 07:15:08 +0000

    Create app shared directory before the log symlink

--- cookbooks/app-logs/recipes/default.rb
+++ cookbooks/app-logs/recipes/default.rb
@@ -15,6 +15,14 @@
     recursive true
   end
 
+  directory "create shared directory to symlink log" do
+    path "/data/#{app_name}/shared"
+    owner node["owner_name"]
+    group node["owner_name"]
+    mode 0755
+    recursive true
+  end
+
   link "/data/#{app_name}/shared/log" do
     to "/var/log/engineyard/apps/#{app_name}"
     owner node["owner_name"]
