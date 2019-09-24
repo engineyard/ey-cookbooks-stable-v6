16147c3fb6c8efd94a9ee90eea039748ec910c62
Dimitris Dalianis <ddalianis@engineyard.com>
2019-09-24 12:03:40 +0300

    indent

--- custom-cookbooks/shared_db/cookbooks/shared_db/recipes/default.rb
+++ custom-cookbooks/shared_db/cookbooks/shared_db/recipes/default.rb
@@ -15,7 +15,7 @@ if apps && parent_app
       mode '0755'
       action :create
     end
-	execute "Symlink #{parent_app_path} to /data/#{app}/shared/config/database.yml" do
+    execute "Symlink #{parent_app_path} to /data/#{app}/shared/config/database.yml" do
       command "ln -sf #{parent_app_path} /data/#{app}/shared/config/database.yml"
       only_if "test -f #{parent_app_path}"
     end
