74600aad00972b1554d6377979e1deb2fdd00a93
Dimitris Dalianis <ddalianis@engineyard.com>
2019-10-01 11:44:36 +0300

    More chef compliant commit

--- cookbooks/nodejs/recipes/yarn.rb
+++ cookbooks/nodejs/recipes/yarn.rb
@@ -1,14 +1,9 @@
-execute "add key from yarn repository" do
-  command "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
-  not_if 'apt-key adv --list-public-key --with-fingerprint --with-colons | grep Yarn -q'
+apt_repository 'yarn' do
+  uri 'https://dl.yarnpkg.com/debian/'
+  components ['main', 'stable']
+  key 'https://dl.yarnpkg.com/debian/pubkey.gpg'
+  action :add
 end
-
-file "apt source yarn" do
-  path "/etc/apt/sources.list.d/yarn.list"
-  content "deb https://dl.yarnpkg.com/debian/ stable main"
-  notifies :run, "execute[update-apt]", :immediately
-end
-
 
 package "yarn" do
   action :install
