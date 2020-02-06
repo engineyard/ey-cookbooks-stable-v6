ca5a57050de9ea06886d466bfaa921580c2aec58
Johann Fuechsl <johann@fuechsl.co>
2020-02-06 13:09:03 +0100

    Refactored and improved spec helpers

--- spec/dna/dna_helper.rb
+++ spec/dna/dna_helper.rb
@@ -1,18 +1,32 @@
 require 'dnapi'
+require 'base64'
 
-def set_dna(node, dna_name)
-  dna_file = File.expand_path("../dna_files/#{dna_name}.json", __FILE__)
-  set_dna_from_file(node, dna_file)
-end
+class DNASpec
+  def self.build_instance(environment, role, extra_opts = {})
+    id = environment.instances.length + 1
+    environment.build_instance({
+      :id               => id,
+      :public_hostname  => "public_host.#{id}",
+      :private_hostname => "private_host.#{id}",
+      :role             => role
+    }.merge(extra_opts))
+  end
 
-def set_dna_from_file(node, dna_file)
-  dna = JSON.parse(IO.read(dna_file))
-  yield dna if block_given?
-  node.default['dna'] = dna
-end
+  def self.set_app_environment_variables(app, env_vars)
+    env_vars = env_vars.map do |var|
+      var[:value] = ::Base64.strict_encode64(var[:value])
+      var
+    end
+    app.add_component(:app_metadata, environment_variables: env_vars)
+  end
 
-def set_dnapi_environment(node)
-  dna = DNApi.build
-  yield dna if block_given?
-  node.default['dna'] = dna.to_hash
+  def self.set_dnapi_environment(node)
+    environment = DNApi.build
+    if block_given?
+      instance = yield environment
+    else
+      instance = build_instance(environment, :solo)
+    end
+    node.normal['dna'] = instance.to_dna
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,22 @@ require 'chefspec'
 
 require_relative 'dna/dna_helper'
 
+def create_chef_runner
+  ChefSpec::SoloRunner.new do |node|
+    if block_given?
+      DNASpec::set_dnapi_environment(node) do |environment|
+        yield environment
+      end
+    else
+      DNASpec::set_dnapi_environment(node)
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.platform = 'ey-ubuntu'
   config.version = '18.04.0.9'
+  config.cookbook_path = File.expand_path('../../cookbooks', __FILE__)
 end
 
 at_exit { ChefSpec::Coverage.report! }
