2d1b0092dfb3bd1c265b92c2636f011449906797
Johann Fuechsl <johann@fuechsl.co>
2020-02-11 11:25:17 +0100

    Fix a bug extracting PHP version component

--- cookbooks/php/attributes/php.rb
+++ cookbooks/php/attributes/php.rb
@@ -1,4 +1,7 @@
-default['php']['version'] = case attribute['dna']['engineyard']['environment']['components'].map(&:values).flatten.find(/^php_/).first
+php_component_version = attribute['dna']['engineyard']['environment']['components']
+  .map(&:values).flatten.find { |cv| cv =~ /^php_/ }
+
+default['php']['version'] = case php_component_version.to_s
   when 'php_71'
     '7.1'
   when 'php_72'
@@ -12,4 +15,4 @@ default['php']['version'] = case attribute['dna']['engineyard']['environment']['
 end
 
 default['php']['known_versions'] = %w[ 7.1 7.2 7.3 7.4 ]
-default['php']['minor_version'] =  default['php']['version'].split(".").first(2).join(".")
+default['php']['minor_version'] =  default['php']['version']
