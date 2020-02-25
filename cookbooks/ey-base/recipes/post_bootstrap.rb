8020875eae5900e576283dd0fb2a213bf06e0120
Callum Pease <cpease1997@hotmail.com>
2020-02-25 19:52:31 +0000

    Update post_bootstrap.rb

--- cookbooks/ey-base/recipes/post_bootstrap.rb
+++ cookbooks/ey-base/recipes/post_bootstrap.rb
@@ -1,6 +1,6 @@
 include_recipe "monit"
 include_recipe "collectd"
-include_recipe "php"
+include_recipe "php" if node['dna']['applications'].first[1]['type'] == 'php'
 include_recipe "nodejs::common"
 include_recipe "nodejs::yarn"
 include_recipe "reboot"
