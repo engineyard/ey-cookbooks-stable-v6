739d0516554fce0c9716f03c0cb588e25d1b5cc2
Johann Fuechsl <johann@fuechsl.co>
2020-03-23 16:44:53 +0100

    Fix logic for including recipe mysql::setup_app_users_dbs

--- cookbooks/app/recipes/build.rb
+++ cookbooks/app/recipes/build.rb
@@ -1,5 +1,5 @@
 include_recipe "db_admin_tools"
 
-if db_host_is_rds? && node.engineyard.environment[:db_stack_name][/mysql/]
+if db_host_is_rds? && node.engineyard.environment[:db_stack_name][/^(mysql\d+|aurora\d+)/]
   include_recipe "mysql::setup_app_users_dbs"
 end
