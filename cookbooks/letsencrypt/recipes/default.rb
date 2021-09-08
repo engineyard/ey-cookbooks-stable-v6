4878e4d98ed159ad694e3e33c65b34d790059b1d
Callum Pease <cpease@engineyard.com>
2021-09-08 12:03:35 +0100

    Adds wildcard letsencrypt

--- cookbooks/letsencrypt/recipes/default.rb
+++ cookbooks/letsencrypt/recipes/default.rb
@@ -1,13 +1,24 @@
 letsencrypt = node['letsencrypt']
 
-package "certbot" do
+package "python3-pip" do
   action :install
 end
 
-md = fetch_env_var(node, 'EY_LE_MAIN_DOMAIN').downcase
+execute "ensure pip3 is up-to-date" do
+  command "pip3 install pip --upgrade"
+end
+
+execute "install certbot" do
+  command "pip3 install certbot"
+end
+
+md = fetch_env_var(node, 'EY_LE_DOMAINS').split[0]  #= fetch_env_var(node, 'EY_LE_MAIN_DOMAIN').downcase
 domain = fetch_env_var(node, 'EY_LE_DOMAINS').nil? || (fetch_env_var(node, 'EY_LE_DOMAINS').gsub(" "," -d ")).downcase
 app = fetch_env_var(node, 'EY_LE_MAIN_APP_NAME') || node['dna']['applications'].keys.first
 wc = fetch_env_var(node, 'EY_LE_USE_WILDCARD') || false
+type = fetch_env_var(node, 'EY_LE_DNS_TYPE') || ""
+
+Chef::Log.info "LetsEncrypt Widlcard: #{wc}"
 
 if Dir.exist?("/data/#{app}/current") && ['solo', 'app_master'].include?(node['dna']['instance_role'])
  
@@ -15,13 +26,36 @@ if Dir.exist?("/data/#{app}/current") && ['solo', 'app_master'].include?(node['d
     command "/etc/init.d/haproxy start /etc/init.d/nginx start"
   end 
 
-if !wc
+  if wc
 
-  execute "issue certificate" do
-    command "certbot certonly --noninteractive --agree-tos --register-unsafely-without-email -d #{domain} --webroot -w /data/#{app}/current/public/"
-    not_if { ::Dir.exist? ("/etc/letsencrypt/live/#{md}/") }
+    execute "Install plugin for certbot" do
+      command "pip3 install certbot-dns-#{type}"
+    end
+
+    managed_template "/opt/.letsencrypt-secrets" do
+      mode 0700
+      source "letsencrypt.secrets.erb"
+      variables(
+        :data => fetch_env_var(node, 'EY_LE_DNS_AUTH_INFO')
+      )
+    end
+
+    execute "Issue certiciate initially" do
+      command "certbot certonly --dns-#{type} --dns-#{type}-credentials /opt/.letsencrypt-secrets -d #{domain} --non-interactive --agree-tos --register-unsafely-without-email --dry-run"
+      not_if { File.exist?("/etc/letsencrypt/live/#{md}/privkey.pem") }
+    end
+
   end
-end 
+
+  if !wc
+
+    execute "issue certificate" do
+      command "certbot certonly --noninteractive --agree-tos --register-unsafely-without-email -d #{domain} --webroot -w /data/#{app}/current/public/"
+      not_if { ::Dir.exist? ("/etc/letsencrypt/live/#{md}/") }
+    end
+  end 
+
+
 
   managed_template "/engineyard/bin/copycerts.sh" do
     owner 'root'
@@ -30,9 +64,22 @@ end
     source "copycerts.sh.erb"
     variables(   
       :app_name => app,
-      :instances => (node.cluster - node.db_slaves - node.db_master).join(' '),
+      :instances => (node.cluster - node.db_slaves - node.db_master - node.util_servers).join(' '),
       :md => md
     )
+  end
+
+  managed_template "/engineyard/bin/check_le.sh" do
+    owner 'deploy'
+    group 'deploy'
+    mode 0700
+    source 'check_le.sh.erb'
+    variables(
+    :md => md
+    )
+  end
+  execute "update collectd" do
+    command 'sed -i \'81 i \ \ Exec \"deploy\" \"/engineyard/bin/check_le.sh\"\' /etc/engineyard/collectd.conf && /etc/init.d/collectd restart'
   end
 
   execute "push certificate" do
